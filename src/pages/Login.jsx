2fb653157515a16051a2608c12d6a68b51e8d8e4
Amos, Arthur, Merveille <nyirendaamos1@gmail.com>
2025-04-29 19:09:23 +0100

    updated the login and register pages

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,44 @@
 import "../styles/Login.css";
+import { useState } from "react";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = () => {
+    e.preventDefault();
+    const response = axios.post("http://localhost:8000/api/auth/login", {
+      email,
+      password,
+    });
+
+    if (response.status === 200) {
+      alert("Login successful!");
+      window.location.href = "/home";
+    }
+  };
+
   return (
-    <div className='form-container'>
-      <form className='form-box'>
+    <div className="form-container">
+      <form className="form-box" onSubmit={handleSubmit}>
         <h2>Login Page</h2>
-        <input type='email' placeholder='Email' required />
-        <input type='password' placeholder='Password' required />
-        <button type='submit'>Sign In</button>
-        <p className='switch-text'>
-          Don't have an account? <a href='/sign-up'>Register</a>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button type="submit">Sign In</button>
+        <p className="switch-text">
+          Don't have an account? <a href="/sign-up">Register</a>
         </p>
       </form>
     </div>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,16 +1,66 @@
 import "../styles/Login.css";
+import { useState } from "react";
 
 const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [name, setName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      alert("Passwords do not match!");
+      return;
+    }
+    const response = axios.post("http://localhost:8000/api/auth/register", {
+      name,
+      email,
+      password,
+      confirmPassword,
+    });
+
+    if (response.status === 200) {
+      alert("Registration successful!");
+      window.location.href = "/login";
+    }
+  };
+
   return (
-    <div className='form-container'>
-      <form className='form-box'>
+    <div className="form-container">
+      <form className="form-box" onSubmit={handleSubmit}>
         <h2>Register</h2>
-        <input type='text' placeholder='Full Name' required />
-        <input type='email' placeholder='Email' required />
-        <input type='password' placeholder='Password' required />
-        <button type='submit'>Sign Up</button>
-        <p className='switch-text'>
-          Already have an account? <a href='/sign-in'>Login</a>
+        <input
+          type="text"
+          placeholder="Full Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Confirm Password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          required
+        />
+        <button type="submit">Sign Up</button>
+        <p className="switch-text">
+          Already have an account? <a href="/sign-in">Login</a>
         </p>
       </form>
     </div>
