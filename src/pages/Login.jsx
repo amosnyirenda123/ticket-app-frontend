d29b1616948c128c738a23603674afff32a972a9
Amos Nyirenda <nyirendaamos1@gmail.com>
2025-05-02 10:02:32 +0100

    testing docker hub 2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -22,7 +22,7 @@ const Login = () => {
   return (
     <div className="form-container">
       <form className="form-box" onSubmit={handleSubmit}>
-        <h2>Login Form</h2>
+        <h2>Login Form Test</h2>
         <input
           type="email"
           placeholder="Email"
